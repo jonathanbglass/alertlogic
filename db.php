27d4214419715bcb917d42ac22e8309987831647
jonathanbglass <jonathan.glass@gmail.com>
2017-03-22 14:30:08 -0400

    Delete db.php
    not ready for this file yet

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php 
-function getdb() {
-    $conn_string = "host=dbhost dbname=db user=php_readonly password=passwod";
-      $db = pg_connect($conn_string) or die('connection failed');
-      return $db;
-}
-
-?>
